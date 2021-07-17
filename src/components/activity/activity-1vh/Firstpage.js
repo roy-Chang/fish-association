2dc4d727eac735fd4f00c70ccc1938e340052268
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-07-17 21:41:39 +0800

    fix activity 1vh css styles

--- src/components/activity/activity-1vh/Firstpage.js
+++ src/components/activity/activity-1vh/Firstpage.js
@@ -5,7 +5,6 @@ import { BgGreen } from "./styled";
 function Firstpage() {
   return (
     <>
-      <nav style={{ height: "65px", background: "darkblue" }}></nav>
       <BgGreen>
         <header className="banner d-flex">
           <Banner />
