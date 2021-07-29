1a226ad41c198625f2fece5a990960ee5ffb9b0b
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-07-29 12:54:10 +0800

    added signupOrder route for activityPage, i can changed the URL but failed to change the frame, please help me check it out

--- src/pages/ActivityPage.js
+++ src/pages/ActivityPage.js
@@ -1,26 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Firstpage from "../components/activity/activity-1vh/Firstpage";
 import Secondpage from "../components/activity/activity-2vh/Secondpage";
 import Threepage from "../components/activity/activity-3vh/Threepage";
 import Forthpage from "../components/activity/activity-4vh/Forthpage";
 import Fifthpage from "../components/activity/activity-5vh/Fifthpage";
-import MainFooter from '../components/footer/MainFooter'
+import MainFooter from "../components/footer/MainFooter";
 
 import "../assets/css/styled.css";
 import "react-bootstrap"; //Toast
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import FirstOrder from "../components/activity/activity-order/FirstStep/FirstOrder";
+import SecondOrder from "../components/activity/activity-order/SecondStep/SecondOrder";
+import ThirdOrder from "../components/activity/activity-order/ThirdStep/ThirdOrder";
+
 function ActivityPage() {
   return (
     <>
-      <div className="activity-wrapper activity-bg-green">
-        <Firstpage />
-        <Secondpage />
-        <Threepage />
-        <Forthpage />
-        <Fifthpage />
-        <MainFooter />
-      </div>
+      <Router>
+        <div className="activity-wrapper activity-bg-green">
+          <Firstpage />
+          <Secondpage />
+          <Threepage />
+          <Forthpage />
+          <Fifthpage />
+          <MainFooter />
+          <Route path="/signupFirst" component={FirstOrder} />
+          <Route path="/signupSecond" component={SecondOrder} />
+          <Route path="/signupThird" component={ThirdOrder} />
+        </div>
+      </Router>
     </>
   );
 }
