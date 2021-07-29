21e39af420db6d3d2b71af1557b0e71f43b7c2cd
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-07-29 22:57:32 +0800

    changed products banner css styles and router

--- src/components/products/products-list/RankTable.js
+++ src/components/products/products-list/RankTable.js
@@ -1,21 +1,21 @@
 import React from "react";
-import RankCard from "./RankCard"
+import RankCard from "./RankCard";
 import { FaFireAlt } from "react-icons/fa";
 
-
 export default function RankTable() {
-    return (
-      <>
-        <div className="P-rank-title d-flex align-items-center mt-5 mb-1">
-            <FaFireAlt className="mr-1" />
-            <p style={{margin:0,fontWeight:"bold"}}>熱門排行</p>
-        </div>
-        <div className="d-flex flex-column">
-          <RankCard />
-          <RankCard />
-          <RankCard />
-        </div>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div className="P-rank-title d-flex align-items-center mt-5 mb-1">
+        <FaFireAlt className="mr-1" />
+        <p style={{ margin: 0, fontWeight: "bold", padding: "5px" }}>
+          熱門排行
+        </p>
+      </div>
+      <div className="d-flex flex-column">
+        <RankCard />
+        <RankCard />
+        <RankCard />
+      </div>
+    </>
+  );
+}
