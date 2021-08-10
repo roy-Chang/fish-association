dc3b69e0808b30c29bce6a2fb82b316ddd326278
roy-Chang <83852512+roy-Chang@users.noreply.github.com>
2021-08-10 14:43:15 +0800

    fixed

--- src/components/products/products-list/RankCard.js
+++ src/components/products/products-list/RankCard.js
@@ -4,6 +4,9 @@ import {Card} from 'react-bootstrap';
 /* img import */
 import { FaShoppingCart } from "react-icons/fa";
 import saira01 from "../../../assets/img/products/fish/saira01.jpg"
+import eel from '../../../assets/img/products/fish/eel1.jpg'
+import lobster1 from '../../../assets/img/products/shrimp/lobster1.jpg'
+
 
 export default function RankTable() {
     return (
@@ -12,11 +15,35 @@ export default function RankTable() {
             <Card.Img variant="top" src={saira01}  className="P-rank-card-img"/>
             <Card.Body>
                 <div className="d-flex justify-content-between align-items-center">
-                    <Card.Title>沙丁魚</Card.Title>
-                    <FaShoppingCart style={{width:"25px",height:"25px"}} />
+                    <Card.Title>秋刀魚</Card.Title>
+                    {/* <FaShoppingCart style={{width:"25px",height:"25px"}} /> */}
                 </div>
                 <Card.Text>
-                    定價 $ 1000
+                    定價 $ 300
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        <Card className="P-rank-card my-2" >
+            <Card.Img variant="top" src={eel}  className="P-rank-card-img"/>
+            <Card.Body>
+                <div className="d-flex justify-content-between align-items-center">
+                    <Card.Title>蒲燒鰻</Card.Title>
+                    {/* <FaShoppingCart style={{width:"25px",height:"25px"}} /> */}
+                </div>
+                <Card.Text>
+                    定價 $ 500
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        <Card className="P-rank-card my-2" >
+            <Card.Img variant="top" src={lobster1}  className="P-rank-card-img"/>
+            <Card.Body>
+                <div className="d-flex justify-content-between align-items-center">
+                    <Card.Title>龍蝦</Card.Title>
+                    {/* <FaShoppingCart style={{width:"25px",height:"25px"}} /> */}
+                </div>
+                <Card.Text>
+                    定價 $ 600
                 </Card.Text>
             </Card.Body>
         </Card>
